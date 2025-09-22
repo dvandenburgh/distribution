3510d2a5cd8ade1868ac541133b00147f81d23d8
rocknix <macebrooks@gmail.com>
2025-09-22 20:56:52 +0000

    SDM845: Fix build

--- projects/ROCKNIX/packages/linux/package.mk
+++ projects/ROCKNIX/packages/linux/package.mk
@@ -290,7 +290,6 @@ make_target() {
           ;;
       esac
 
-      #[ "${DEVICE}" != "RK3588" ] && export BUILD_BPF_SKEL=0
       [ "${DEVICE}" != "RK3588" && "${DEVICE}" != "SDM845" ] && export BUILD_BPF_SKEL=0
 
 

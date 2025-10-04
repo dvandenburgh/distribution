5b10ba1bc1e6b04341ae5838b74fdd0f29d765f7
spycat88 <spycat88@users.noreply.github.com>
2025-10-04 17:36:27 +0100

    rocknix-splash: move from initramfs to system

--- projects/ROCKNIX/packages/tools/rocknix-splash/package.mk
+++ projects/ROCKNIX/packages/tools/rocknix-splash/package.mk
@@ -2,9 +2,22 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-splash"
-PKG_VERSION="1.0.3"
+PKG_VERSION="c0e9df5e50b217a76ed86667948d49e1654c3a36"
+#PKG_VERSION="868e60b090081f71b254007644661925f8c875df" #(src)
 PKG_LICENSE="GPL"
 PKG_SITE="https://rocknix.org"
 PKG_URL="https://github.com/ROCKNIX/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
+#PKG_URL="https://github.com/ROCKNIX/rocknix-splash-src.git"
 PKG_DEPENDS_INIT="toolchain"
 PKG_LONGDESC="ROCKNIX splash screen application"
+PKG_TOOLCHAIN="manual"
+#PKG_USETOKEN="yes"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+    cp -v rocknix-splash ${INSTALL}/usr/bin
+}
+
+post_install() {
+  enable_service rocknix-splash.service
+}

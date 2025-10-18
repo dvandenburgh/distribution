edab3e75eb38d01b8e63fcab7ed9524bbdd17263
rocknix <macebrooks@gmail.com>
2025-10-18 12:50:20 +0000

    Update kernel to 6.17.3 and Mesa to 25.2.5, rollback dolphin to stable

--- projects/ROCKNIX/packages/emulators/standalone/dolphin-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/dolphin-sa/package.mk
@@ -10,7 +10,7 @@ PKG_TOOLCHAIN="cmake"
 
 case ${DEVICE} in
   SM8250|SM8550|SDM845|AMD64|RK3399)
-    PKG_VERSION="79614956f39fc1f6d47ff3fcf6484ef19f4e1ae4"
+    PKG_VERSION="ba7bf19b102d1b27f594c780c9d75970a276213a"
     PKG_SITE="https://github.com/dolphin-emu/dolphin"
     PKG_URL="${PKG_SITE}.git"
     PKG_DEPENDS_TARGET+=" qt6"
